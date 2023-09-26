d8b6ba521cc63aee8cb77db79ca993cbd40aaf35
nahirniy <115656838+nahirniy@users.noreply.github.com>
2023-09-26 14:07:55 +0000

    Deploying to gh-pages from @ nahirniy/goit-js-hw-09@ad42263c87b3b8ea4bbb25a4e55bb785650d2a52 🚀

---
+++ 01-color-switcher.42a6ef0c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){t.setAttribute("disabled","true");const o=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3);e.addEventListener("click",(()=>{clearInterval(o),t.removeAttribute("disabled")}))}));
+//# sourceMappingURL=01-color-switcher.42a6ef0c.js.map
